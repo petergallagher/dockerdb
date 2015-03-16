90c321f0b07e8bd20a8ccc3a9273c1b86d946b7c
Peter Gallagher <peter.s.gallagher@gmail.com>
2015-03-16 15:44:21 +0000

    Remove second create user

--- import.sh
+++ import.sh
@@ -2,7 +2,6 @@
 
 mysql -u root -h $OPEN_EYES_DB_HOST -e "CREATE DATABASE $OPEN_EYES_DB_DATABASE"
 mysql -u root -h $OPEN_EYES_DB_HOST -e "CREATE USER '$OPEN_EYES_DB_USER' IDENTIFIED BY '$OPEN_EYES_DB_PASS'"
-mysql -u root -h $OPEN_EYES_DB_HOST -e "CREATE USER '$OPEN_EYES_DB_USER';"
 mysql -u root -h $OPEN_EYES_DB_HOST -e "grant all on $OPEN_EYES_DB_DATABASE.* to '$OPEN_EYES_DB_USER'@'%' identified by '$OPEN_EYES_DB_PASS'"
 
 echo "Created user $OPEN_EYES_DB_USER on host $OPEN_EYES_DB_HOST with pass $OPEN_EYES_DB_PASS"
