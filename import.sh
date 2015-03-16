dc5d212cf5365e75966153b29d8d7e2fe5f53ed7
Peter Gallagher <peter.s.gallagher@gmail.com>
2015-03-16 15:26:24 +0000

    Fix typo

--- import.sh
+++ import.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mysql -u root -h $OPEN_EYES_DB_HOST -e "CREATE DATABSE $OPEN_EYES_DB_DATABASE"
+mysql -u root -h $OPEN_EYES_DB_HOST -e "CREATE DATABASE $OPEN_EYES_DB_DATABASE"
 mysql -u root -h $OPEN_EYES_DB_HOST -e "CREATE USER '$OPEN_EYES_DB_USER' IDENTIFIED BY '$OPEN_EYES_DB_PASS'"
 mysql -u root -h $OPEN_EYES_DB_HOST -e "CREATE USER '$OPEN_EYES_DB_USER';"
 mysql -u root -h $OPEN_EYES_DB_HOST -e "grant all on $OPEN_EYES_DB_DATABASE.* to '$OPEN_EYES_DB_USER'@'%' identified by '$OPEN_EYES_DB_PASS'"
