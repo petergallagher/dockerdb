a8032ec78733b5748f30d20ab614212b94f7edbb
Peter Gallagher <peter.s.gallagher@gmail.com>
2015-06-15 17:02:28 +0100

    Remove mysql start

--- import.sh
+++ import.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
-UP=$(pgrep mysql | wc -l);
-if [ "$UP" -gt 0 ];
-then
-    echo "=> Starting MySQL Server"
-    /usr/bin/mysqld_safe > /dev/null 2>&1 &
-fi
 
-PID=$!
-
-RET=1
-while [[ RET -ne 0 ]]; do
-    echo "=> Waiting for confirmation of MySQL service startup"
-    sleep 5
-    mysql -u root -h $OPEN_EYES_DB_HOST -e "status" > /dev/null 2>&1
-RET=$?
-done
-
-echo "   Started with PID ${PID}"
 
 mysql -u root -h $OPEN_EYES_DB_HOST -e "CREATE DATABASE $OPEN_EYES_DB_DATABASE"
 mysql -u root -h $OPEN_EYES_DB_HOST -e "CREATE USER '$OPEN_EYES_DB_USER' IDENTIFIED BY '$OPEN_EYES_DB_PASS'"
